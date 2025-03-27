8bbb2e12378a3507c21caf09f467ae039d677019
Neto Russo <netoacrn1@gmail.com>
2025-03-27 03:22:24 -0300

    Subindo a parcial

---
+++ src/components/Desafio/index.jsx
@@ -0,0 +1,147 @@
+import styled from "styled-components";
+import ReactEcharts from "echarts-for-react";
+
+const Container = styled.section`
+  width: 90%;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box1 = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const BoxGrafico = styled.div`
+  width: 700px;
+  height: 750px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BoxDados = styled.div`
+  width: 700px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const ItemDado = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-size: 16px;
+`;
+
+const Cor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+`;
+
+const BoxPorcentagem = styled.span`
+  background-color: black;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 10px;
+  font-size: 14px;
+`;
+
+const BoxLegenda = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  font-size: 14px;
+`;
+
+const dados = [
+  { nome: "Até 24 horas", value: 1250, porcentagem: 50, cor: "#f4a742" },
+  { nome: "de 24 a 48 horas", value: 375, porcentagem: 40, cor: "#2a9df4" },
+  { nome: "Mais de 48 horas", value: 125, porcentagem: 10, cor: "#004466" },
+];
+
+const options = {
+
+  visualMap: {
+    show: false,
+    min: 0,
+    max: 1250,
+  },
+  series: [
+    {
+      type: "pie",
+      stillShowZeroSum: false,
+      center: ["50%", "50%"],
+      padAngle: 4,
+      startAngle: 90,
+      endAngle: 360 + 2,
+      itemStyle: {
+        offset: [15, 15],
+      },
+      data: dados
+        .map((item) => ({
+          value: item.porcentagem,
+          radius: ['0%', `${(item.porcentagem * 0.01 + 0.5) * 45}%`],
+          name: item.nome,
+          itemStyle: {
+            color: item.cor,
+            borderWidth: item.porcentagem === 10 ? 1 : item.porcentagem === 40 ? 10 : 15,
+            borderColor: "#fff",
+            offset: [item.porcentagem , item.porcentagem * 0.8],
+          },
+        }))
+        .sort((a, b) => a.value - b.value),
+      label: {
+        show: false,
+      },
+      labelLine: {
+        show: false,
+      },
+      startAngle: 0,
+      minAngle: 1,
+    },
+  ],
+
+};
+
+const Desafio = () => {
+  return (
+    <Container>
+      <Box1>
+
+        <BoxGrafico>
+          <ReactEcharts option={options}
+            style={{ width: "100%", height: "500px" }}
+          />
+        </BoxGrafico>
+
+        <BoxDados>
+          {dados.map((item, index) => (
+            <ItemDado key={index}>
+              <Cor style={{ backgroundColor: item.cor }} />
+              {item.nome} <strong>{item.value}</strong> <BoxPorcentagem>{item.porcentagem}%</BoxPorcentagem>
+            </ItemDado>
+          ))}
+        </BoxDados>
+      </Box1>
+
+      <BoxLegenda>
+
+      </BoxLegenda>
+    </Container>
+  );
+};
+
+export default Desafio;
