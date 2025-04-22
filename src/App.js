d5364f2feebf82d7eda038acf51120d236c434d5
Neto Russo <netoacrn1@gmail.com>
2025-04-22 09:43:46 -0300

    Finally: Desafio 03

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Desafio from "./components/Desafio";
 import Desafio02 from "./components/Desafio02";
+import Desafio03 from "./components/Desafio03";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Desafio />
       <Desafio02 />
+      <Desafio03 />
     </div>
   );
 }
